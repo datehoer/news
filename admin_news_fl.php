9ab1cbbe837977b7c9a8e3fa0c08379071ba29b5
datehoer <datehoer@gmail.com>
2021-06-07 15:59:21 +0800

    Chanage class fl

--- admin_news_fl.php
+++ admin_news_fl.php
@@ -12,7 +12,7 @@
     <title>China-news-admin</title>
     <link rel="stylesheet" href="../news/css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-olOxEXxDwd20BlATUibkEnjPN3sVq2YWmYOnsMYutq7X8YcUdD6y/1I+f+ZOq/47" crossorigin="anonymous">
-    
+    <link rel='stylesheet' href="../news/css/fl-css.css">
 </head>
 <body>
     <nav>
@@ -24,11 +24,13 @@
             <li><a href="admin_index.php">后台管理</a></li>
         </ul>
     </nav>
-    <div class="addclass">
-        <a href="news_class_add.php">添加新闻类别</a>
-    </div>
-    <div class="editclass">
-        <a href="news_class_delete.php">修改新闻类别</a>
+    <div class="classes-box">
+        <div class="addclass class-box">
+            <a href="news_class_add.php">添加新闻类别</a>
+        </div>
+        <div class="editclass class-box">
+            <a href="news_class_delete.php">修改新闻类别</a>
+        </div>
     </div>
 </body>
 </html>
